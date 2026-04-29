8df2d8587535e6ca0055a8c162166b66b3cccf7a
adhiraj-code <alkarathore87@gmail.com>
2026-04-29 19:43:17 +0530

    Add files via upload

---
+++ Tuple Products.py
@@ -0,0 +1,9 @@
+my_tuple = (2, 3, 4, 5)
+
+def multiply_tuple_elements(tuple):
+    result = 1
+    for i in tuple:
+        result = result * 1
+        return result
+    
+print(multiply_tuple_elements(my_tuple))
